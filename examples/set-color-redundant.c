d17f41fbc8af745192ffb6fb40a5c0ccd1412143
Brendan Caffrey <brendan@jcaffrey.com>
2016-09-10 20:35:28 -0500

    set-[color/power]-redundant take an optional list of light ids

--- examples/set-color-redundant.c
+++ examples/set-color-redundant.c
@@ -1,23 +1,36 @@
 #include <stdio.h>
 #include "../sender.h"
 #include "../color.h"
+#include "../types.h"
+#include "helpers.h"
 
+#define NUM_REQUIRED_ARGS 6
+#define INITIAL_SEQNUM 120
 int main(int argc, char** argv)
 {
-    if (argc != 6) {
-        fprintf(stderr, "Usage: %s h s b k duration\n", argv[0]);
+    if (argc < NUM_REQUIRED_ARGS) {
+        fprintf(stderr, "Usage: %s h s b k duration [optional list of light ids]\n", argv[0]);
         exit(2);
     }
 
+    int num_lights = 0;
+    uint64_t* light_ids = NULL;
+    parse_light_ids(argc, argv, NUM_REQUIRED_ARGS, &num_lights, &light_ids);
+
     struct lifx_lan_sender s;
     lifx_lan_sender_init(&s);
-    s.messages.seqnum = 120;
-    struct lifx_lan_light_color color;
-    lifx_lan_color_hsbk(&color, atoi(argv[1]), atoi(argv[2]), atoi(argv[3]), atoi(argv[4]));
 
-    lifx_lan_sender_light_set_color(&s, LIFX_LAN_TARGET_ALL, &color, atoi(argv[5]));
-    lifx_lan_sender_light_set_color(&s, LIFX_LAN_TARGET_ALL, &color, atoi(argv[5]));
+    struct lifx_lan_light_color color;
+    lifx_lan_color_hsbk(&color, strtol(argv[1], NULL, 10), strtol(argv[2], NULL, 10), strtol(argv[3], NULL, 10), strtol(argv[4], NULL, 10));
+    uint32_t duration = strtol(argv[5], NULL, 10);
+
+    for (int i = 0; i < num_lights; ++i) {
+        s.messages.seqnum = INITIAL_SEQNUM + i;
+        lifx_lan_sender_light_set_color(&s, light_ids[i], &color, duration);
+        lifx_lan_sender_light_set_color(&s, light_ids[i], &color, duration);
+    }
 
     lifx_lan_sender_uninit(&s);
+    free(light_ids);
     return 0;
 }
--- examples/set-power-redundant.c
+++ examples/set-power-redundant.c
@@ -1,20 +1,30 @@
 #include <stdio.h>
 #include "../sender.h"
+#include "helpers.h"
 
+#define NUM_REQUIRED_ARGS 2
+#define INITIAL_SEQNUM 220
 int main(int argc, char** argv)
 {
-    if (argc != 2 || (argv[1][0] != '0' && argv[1][0] != '1')) {
+    if (argc < NUM_REQUIRED_ARGS || (argv[1][0] != '0' && argv[1][0] != '1')) {
         fprintf(stderr, "Usage: %s [0/1]\n", argv[0]);
         exit(2);
     }
 
+    int num_lights = 0;
+    uint64_t* light_ids = NULL;
+    parse_light_ids(argc, argv, NUM_REQUIRED_ARGS, &num_lights, &light_ids);
+
     struct lifx_lan_sender s;
     lifx_lan_sender_init(&s);
-    s.messages.seqnum = 240;
 
-    lifx_lan_sender_device_set_power(&s, LIFX_LAN_TARGET_ALL, argv[1][0] == '1');
-    lifx_lan_sender_device_set_power(&s, LIFX_LAN_TARGET_ALL, argv[1][0] == '1');
+    for (int i = 0; i < num_lights; ++i) {
+        s.messages.seqnum = INITIAL_SEQNUM + i;
+        lifx_lan_sender_device_set_power(&s, light_ids[i], argv[1][0] == '1');
+        lifx_lan_sender_device_set_power(&s, light_ids[i], argv[1][0] == '1');
+    }
 
     lifx_lan_sender_uninit(&s);
+    free(light_ids);
     return 0;
 }
